3d50a5b0364775c96af2604eb10164f1e40875f2
seoyeoning <minalunpark@naver.com>
2025-09-01 00:59:32 +0900

    create project

---
+++ src/main/java/app/booksearch/ParkSeoyeonBooksearchApplication.java
@@ -0,0 +1,13 @@
+package app.booksearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkSeoyeonBooksearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkSeoyeonBooksearchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/app/booksearch/ParkSeoyeonBooksearchApplicationTests.java
@@ -0,0 +1,13 @@
+package app.booksearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkSeoyeonBooksearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
